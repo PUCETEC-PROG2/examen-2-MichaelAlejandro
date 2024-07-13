46f5e1215d229c410f0b76ec4239aea7511ddaa3
Michael Alejandro <maalejandro@puce.edu.ec>
2024-07-13 09:08:46 -0500

    crear modelo

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movies (models.Model):
+    tittle = models.CharField(max_length=50, null=False)
+    gender = models.CharField(max_length=25, null=False)   
+    director = models.CharField(max_length=50, null=False)
+    year_realease = models.IntegerField(null=False)
+    sinopsis = models.TextField(null=False)
+    
+    def __str__(self) -> str:
+        return self.tittle
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+from .models import Movies
+
+def index(request):
+    Movies = Movies.objects.order_by('gender')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movies': movies}, request))
+
+def movie(request, movies_id):
+    movies = Movies.objects.get(pk= movies_id)
+    template = loader.get_template('display_movies.html')
+    context = {
+        'movie': movies
+    }
+    return HttpResponse(template.render(context, request))
